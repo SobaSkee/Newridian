0473ddb0c02decc0fe293c78081744e4cdc2707b
Stanley Ke <kestanley101@gmail.com>
2024-07-24 12:12:44 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,28 +1,23 @@
 
 const menuIcon = document.querySelector('.mobile-nav #open-icon');
 const menuIconPath = document.querySelector(".mobile-nav #menu-icon-path");
-// paths for the open and close buttons
-const openPath = "M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z";
-const closePath = "m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z";
 
 function toggleNav() {
+
+
   const sidebar = document.querySelector('.mobile-nav .sidebar');
   const overlay = document.querySelector('.mobile-nav .overlay');
-  
-  // if (menuIconPath.getAttribute('d') === openPath) {
-  //   menuIconPath.setAttribute('d', closePath);
-  //   sidebar.style.display = "flex";
-  // } 
-  // else {
-  //   menuIconPath.setAttribute('d', openPath);
-  //   sidebar.style.display = "none";
-  // }
+  // paths for the open and close buttons
+  const openPath = "M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z";
+  const closePath = "m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z";
   if (sidebar.style.display === 'none' || sidebar.style.display === '') {
     sidebar.style.display = 'flex';
     overlay.style.display = 'block';
+    menuIconPath.setAttribute('d', closePath);
   } else {
     sidebar.style.display = 'none';
     overlay.style.display = 'none';
+    menuIconPath.setAttribute('d', openPath);
   }
 }
 
