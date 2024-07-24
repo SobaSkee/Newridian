72d97b6740f0bbaaad436497bb1a5a2d1d0fba0a
SobaSkee <117794857+SobaSkee@users.noreply.github.com>
2024-07-24 09:59:11 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
 const sidebar = document.querySelector('.mobile-nav .sidebar');
-const openIcon = document.querySelector('.mobile-nav #open-icon');
-function closeNav() {
-  sidebar.style.display = "none";
-}
+const menuIcon = document.querySelector('.mobile-nav #open-icon');
+const menuIconPath = document.querySelector(".mobile-nav #menu-icon-path");
+// paths for the open and close buttons
+const openPath = "M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z";
+const closePath = "m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z";
 
-function openNav() {
-  sidebar.style.display = "flex";
-
+function toggleNav() {
+  
+  
+  if (menuIconPath.getAttribute('d') === openPath) {
+    menuIconPath.setAttribute('d', closePath);
+    sidebar.style.display = "flex";
+  } 
+  else {
+    menuIconPath.setAttribute('d', openPath);
+    sidebar.style.display = "none";
+  }
 }
 
 function changeColor() {
-  openIcon.setAttribute('fill', '#EF9539');
+  menuIcon.setAttribute('fill', '#EF9539');
 }
 
 function revertColor() {
-  openIcon.setAttribute('fill', 'black');
+  menuIcon.setAttribute('fill', 'black');
 }
